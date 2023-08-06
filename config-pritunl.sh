90a0292b6da180261c6ab9c432ce86d5d5d674ef
Agus Wibawa <106722970+agutama@users.noreply.github.com>
2023-08-06 20:59:57 +0700

    Opensearch (#3)
    * add ansible opensearch
    
    * update ansible hosts
    
    * fixing opensearch multi-node

--- config-pritunl.sh
+++ config-pritunl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source ../demo_env/bin/activate
-ansible-playbook playbooks/pritunl/main.yml -i inventories/dev/hosts -e "HOSTS=vpn" -t config
+ansible-playbook playbooks/pritunl/main.yml -i inventories/dev/hosts -e "HOSTS=pritunl" -t config
--- install-pritunl.sh
+++ install-pritunl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source ../demo_env/bin/activate
-ansible-playbook playbooks/pritunl/main.yml -i inventories/dev/hosts -e "HOSTS=vpn" -t install
+ansible-playbook playbooks/pritunl/main.yml -i inventories/dev/hosts -e "HOSTS=pritunl" -t install
