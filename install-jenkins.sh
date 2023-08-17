9ce200bc06c54fafcc745756b78380efd00bdcf0
agutama <aguswibawabudi@gmail.com>
2023-08-17 10:15:31 +0700

    update jenkins

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source ../demo_env/bin/activate
-ansible-playbook playbooks/jenkins/main.yml -i inventories/dev/hosts -e "HOSTS=vm-dev-sea2-jenkins-1"
+ansible-playbook playbooks/jenkins/main.yml -i inventories/dev/hosts -e "HOSTS=vm-dev-sea2-jenkins-1" -t install
